680a90038179e4290a204fcd158a47a7f021ebda
yishayweb <yishayweb@gmail.com>
2022-01-05 16:43:48 +0200

    feat: hi

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,11 +1,8 @@
-import Color from './atoms/Color'
-import Text from './atoms/Text'
-import Margin from './atoms/Margin'
-import Select from './molecules/Select'
+import Color from "./atoms/Color";
+import Text from "./atoms/Text";
+import Margin from "./atoms/Margin";
+import Select from "./molecules/Select";
 
-export {
-    Text,
-    Color,
-    Margin,
-    Select
-}
+console.log("hi");
+
+export { Text, Color, Margin, Select };
